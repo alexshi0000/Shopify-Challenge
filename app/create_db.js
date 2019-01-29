d87cbbc2d9e95f543a7d3a1911c36734af7ecb32
alexshi0000 <alexshi0000@gmail.com>
2019-01-29 14:20:59 -0500

    cart listings collection in mongodb

--- app/create_db.js
+++ app/create_db.js
@@ -6,9 +6,20 @@ MongoClient.connect(url, function(err, db) {
   console.log("Marketplace created!");
 
   var dbo = db.db("marketplace");
+
+  var callback = () => {
+    dbo.createCollection('shoppingCart', (err, res) => {
+      if (err)
+        throw err;
+      console.log('shopping cart listing created');
+      db.close();
+    })
+  };
+
   dbo.createCollection("products", function(err, res) {
-    if (err) throw err;
+    if (err)
+      throw err;
     console.log("product collection created!");
-    db.close();
+    callback();
   });
 });
